8216afc701f7787daad4a00f7f9f08ebfb86e929
oldmastet <ivans290805@gmail.com>
2024-04-02 10:39:34 +0300

    da

--- siteDV/siteDV/settings.py
+++ siteDV/siteDV/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,5 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+   os.path.join(BASE_DIR, "main/static"),
 ]
