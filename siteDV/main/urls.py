84db4eea260e2fd751e3a2897a8b260d768ee5f0
oldmastet <ivans290805@gmail.com>
2024-05-26 19:31:49 +0300

    alo

--- siteDV/main/urls.py
+++ siteDV/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('about', views.about)
+    path('about', views.about),
+    path('contacts', views.contacts),
 ]
--- siteDV/main/views.py
+++ siteDV/main/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
