19fc5d27e8efa20b7fd5c18827de169af66d5be6
oldmastet <ivans290805@gmail.com>
2024-05-27 00:29:35 +0300

    alo

--- siteDV/main/views.py
+++ siteDV/main/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
     return render(request, 'main/about.html')
