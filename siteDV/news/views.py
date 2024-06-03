7f20cb7920c189804d74d61e44c70e9375ab9096
oldmastet <ivans290805@gmail.com>
2024-06-03 18:46:23 +0300

    alo

--- siteDV/news/views.py
+++ siteDV/news/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
-# Create your views here.
+from django.views.generic import DetailView
+from .forms import ArticlesForm
+
+
 def news_home(request):
     news = Articles.objects.order_by('-date')
-    return render(request,'news/news_home.html', {'news': news})
+    return render(request, 'news/news_home.html', {'news': news})
+
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Форма заполнена неправильно'
+
+
+    form = ArticlesForm()
+
+    data = {'form': form,
+            'error': error
+
+    }
+
+    return render(request, 'news/create.html', data)
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
